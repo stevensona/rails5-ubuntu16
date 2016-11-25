962770d357a09bb55ba27549dca9f38ff3fe15c3
Adam Stevenson <adamstevenson121@gmail.com>
2016-11-25 11:11:04 -0500

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev ruby-dev
+
+# install ruby
+apt-get install -y ruby
+gem install bundler
+
+# install nodejs
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+apt-get install -y nodejs
+
+# install rails latest
+gem install rails
